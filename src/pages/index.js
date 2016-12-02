35db328dcf270506828c114f301d5991cad73f9f
crazydev901 <david90192@outlook.com>
2016-12-02 06:35:01 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+// import {Link} from "gatsby"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+export default () => (
+  <div>
+    <Header></Header>
+    <div className="container">
+      This is where our content will go
+    </div>
+    <Footer></Footer>
+  </div>
+);
