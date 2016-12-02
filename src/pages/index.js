1e3203f0c32e2777336dfa87bc40afe6c721a755
crazydev901 <david90192@outlook.com>
2016-12-02 07:09:44 -0700

    feat(component): added post component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,18 @@ import React from "react"
 // import {Link} from "gatsby"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Post from "../components/Post"
 
 export default () => (
   <div>
-    <Header></Header>
+    <Header />
     <div className="container">
-      This is where our content will go
+      <div className="row justify-content-md">
+        <div className="col-xs-6">
+          <Post />
+        </div>
+      </div>
     </div>
-    <Footer></Footer>
+    <Footer />
   </div>
 );
