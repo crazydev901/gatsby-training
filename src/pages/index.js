6a2d21b1e14b364a370282d85165ddfd3d76d22e
crazydev901 <david90192@outlook.com>
2016-12-02 19:51:52 -0700

    feat(layout): added primary layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,25 @@
 import React from "react"
-// import {Link} from "gatsby"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+
+import PrimaryLayout from "../layouts/PrimaryLayout"
 import Post from "../components/Post"
 
 export default () => (
-  <div>
-    <Header />
-    <div className="container">
-      <div className="row justify-content-md">
-        <div className="col-xs-6">
-          <Post title="This is our first post" excerpt="Wer are writing something to be displayed in our excerpt" />
-          <Post title="This is our first post" excerpt="Wer are writing something to be displayed in our excerpt" />
-          <Post title="This is our first post" excerpt="Wer are writing something to be displayed in our excerpt" />
-          <Post title="This is our first post" excerpt="Wer are writing something to be displayed in our excerpt" />
-        </div>
-      </div>
-    </div>
-    <Footer />
-  </div>
-);
+  <PrimaryLayout column="col-xs-6">
+    <Post
+      title="This is our first post"
+      excerpt="Wer are writing something to be displayed in our excerpt"
+    />
+    <Post
+      title="This is our first post"
+      excerpt="Wer are writing something to be displayed in our excerpt"
+    />
+    <Post
+      title="This is our first post"
+      excerpt="Wer are writing something to be displayed in our excerpt"
+    />
+    <Post
+      title="This is our first post"
+      excerpt="Wer are writing something to be displayed in our excerpt"
+    />
+  </PrimaryLayout>
+)
