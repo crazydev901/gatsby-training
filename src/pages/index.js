46f17a961288768e7fed44cbb55d70b18a56dd8f
crazydev901 <david90192@outlook.com>
2016-12-02 21:31:21 -0700

    feat(graphql): added graphql query into index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,34 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import PrimaryLayout from "../layouts/PrimaryLayout"
 import Post from "../components/Post"
 
-export default () => (
-  <PrimaryLayout column="col-xs-6">
-    <Post
-      title="This is our first post"
-      excerpt="Wer are writing something to be displayed in our excerpt"
-    />
-    <Post
-      title="This is our first post"
-      excerpt="Wer are writing something to be displayed in our excerpt"
-    />
-    <Post
-      title="This is our first post"
-      excerpt="Wer are writing something to be displayed in our excerpt"
-    />
-    <Post
-      title="This is our first post"
-      excerpt="Wer are writing something to be displayed in our excerpt"
-    />
-  </PrimaryLayout>
-)
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <PrimaryLayout column="col-xs-6">
+      <Post
+        title="This is our first post"
+        excerpt="Wer are writing something to be displayed in our excerpt"
+      />
+    </PrimaryLayout>
+  )
+}
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          date
+          keywords
+          image
+        }
+        excerpt
+        html
+      }
+    }
+  }
+`
